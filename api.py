5132d48ab5175d7a048878ca27f12dc93e7d7916
Mark <mark.2406@yandex.ru>
2019-09-29 12:16:39 +0500

    Themes added and Dialog.storage[user_id] replaced on self.storage

--- api.py
+++ api.py
@@ -10,10 +10,14 @@ import logging
 app = Flask(__name__)
 
 
+# Сменить Dialog.storage[user_id] на self.storage и self.response['response']['response'] на self.response
+# Запульнуть в БД еще 3 темки
+
+
 class Dialog:
     storage = {}
 
-    def __init__(self):
+    def __init__(self, user_id):
         self.db = QuestionsModel()
 
         self.response = {
@@ -22,6 +26,7 @@ class Dialog:
             'response': {}
         }
         self.response['response']['end_session'] = False
+        self.storage = Dialog.storage[user_id]
 
     def tell_rules(self):
         self.response['response']['text'] = 'Сперва вам предлагаются 3 темы на вопросы. Сменить их вы можете \
@@ -33,48 +38,60 @@ class Dialog:
     def greeting(self):
         self.response['response']['text'] = 'Привет! Хотите посмотреть правила или начнем играть?'
 
-    def suggest_themes(self, user_id):
-        themes = self.db.get_unique_random_themes(Dialog.storage[user_id]['used_themes'], 3)
-        Dialog.storage[user_id]['used_themes'] += themes
+    def suggest_themes(self):
+        themes = self.db.get_unique_random_themes(self.storage['used_themes'], 3)
+        self.storage['used_themes'] += themes
 
         self.response['response']['text'] = \
             'Выпавшие темы: {}, {}, {}. Играем или хотите сменить темы?'.format(*themes)
 
-    def handle_first_step(self, tokens, user_id):
+    def give_question(self):
+        self.response['response']['text'] = 'Some quest here ^_^'
+
+    def handle_first_step(self, tokens):
         if {'играть'}.intersection(tokens):
-            self.suggest_themes(user_id)
-            Dialog.storage[user_id]['step'] = 2
+            self.suggest_themes()
+            self.storage['step'] = 2
         else:
             self.response['response']['text'] = 'Я вас не понимаю. Скажите глупому боту подоходчивее.'
 
-    def handle_second_step(self, tokens, user_id):
+    def handle_second_step(self, tokens):
         if {'сменить'}.intersection(tokens):
-            self.suggest_themes(user_id)
+            if self.storage['swapped_times'] == 3:
+                self.storage['step'] = 3
+                self.give_question()
+            else:
+                self.suggest_themes()
+                self.storage['swapped_times'] += 1
 
 
 @app.route('/', methods=['POST'])
 def main():
-    dialog = Dialog()
     user_id = request.json['session']['user_id']
     tokens = request.json['request']['nlu']['tokens']
 
     if request.json['session']['new']:
         Dialog.storage[user_id] = {
             'step': 1,
-            'used_themes': []
+            'used_themes': [],
+            'swapped_times': 0
         }
+        dialog = Dialog(user_id)
         dialog.greeting()
 
-    elif {'правила'}.intersection(tokens):
-        dialog.tell_rules()
+    else:
+        dialog = Dialog(user_id)
 
-    elif Dialog.storage[user_id]['step'] == 1:
-        dialog.handle_first_step(tokens, user_id)
+        if {'правила'}.intersection(tokens):
+            dialog.tell_rules()
 
-    elif Dialog.storage[user_id]['step'] == 2:
-        dialog.handle_second_step(tokens, user_id)
+        elif Dialog.storage[user_id]['step'] == 1:
+            dialog.handle_first_step(tokens)
 
-    # HERE WILL BE OTHER STEPS
+        elif Dialog.storage[user_id]['step'] == 2:
+            dialog.handle_second_step(tokens)
+
+        # HERE WILL BE OTHER STEPS
 
     dialog.db.close_connection()
     return json.dumps(dialog.response)
