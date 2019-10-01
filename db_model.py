46b91bb1d051767b8ca757a2a09addc4f446e03f
Mark <mark.2406@yandex.ru>
2019-10-01 21:22:13 +0500

    Get random themes method in db model

--- db_model.py
+++ db_model.py
@@ -29,7 +29,7 @@ class QuestionsModel:
 
         self.conn.commit()
 
-    def set_quests_nums(self, new_quests_nums):  # WiP
+    def set_quests_nums(self, new_quests_nums):
         QuestionsModel.check_params(new_quests_nums, self.costs)
         costs_len = len(self.costs)
 
@@ -45,9 +45,12 @@ class QuestionsModel:
                 dif = self.quests_nums[theme] - new_quests_nums[theme]
                 ids = self.cursor.execute('SELECT id FROM questions WHERE theme=?', (theme,)).fetchall()[-dif:]
                 ids = tuple(map(lambda x: x[0], ids))
-                if self.cursor.execute('SELECT content FROM questions WHERE id IN ({})'.format(('?,' * len(ids))[:-1]),
-                                       ids).fetchall() != [('',)] * len(ids):
+
+                if self.cursor.execute(
+                        'SELECT content FROM questions WHERE id IN ({})'.format(('?,' * len(ids))[:-1]), ids
+                ).fetchall() != [('',)] * len(ids):
                     raise ValueError
+
                 self.cursor.execute('DELETE FROM questions WHERE id IN ({})'.format(('?,' * len(ids))[:-1]), ids)
 
             self.quests_nums[theme] = new_quests_nums[theme]
@@ -71,12 +74,23 @@ class QuestionsModel:
 
         return [filtered.pop(random.randint(0, len(filtered) - 1)) for _ in range(num)]
 
+    def get_random_quests(self, themes):
+        req = {}
+        num = len(self.costs)
+
+        for theme in themes:
+            quests = self.cursor.execute(
+                'SELECT cost, content, answer FROM questions WHERE theme=? AND content != \'\'', (theme,)).fetchall()
+            req[theme] = [quests.pop(random.randint(0, len(quests) - 1)) for _ in range(num)]
+
+        return req
+
     @staticmethod
     def read_settings():
         with open('data_settings.json', encoding='utf-8') as settings:
             return json.load(settings)
 
-    def write_prop_in_settings(self, prop, value):
+    def write_prop_in_settings(self, prop, value):  # Записываются темы с 0 вопросами!!!
         self.settings[prop] = value
         with open('data_settings.json', mode='w', encoding='utf-8') as settings:
             return json.dump(self.settings, settings)
@@ -88,12 +102,14 @@ if __name__ == "__main__":
     q_model.set_quests_nums({
         'Операционные системы': 10,
         'Анатомия': 10,
-        'Настольные игры': 10,
         'Отечественная война': 10,
         'Золотой век литературы': 10,
         'Пословицы': 10,
         'Поговорки': 10,
-        'Анаграммы': 10
+        'Первое оружие': 10,
+        'Добро пожаловать в Рим!': 10
     })
+
+    print(q_model.get_random_quests(['Операционные системы', 'Анатомия']))
 
     q_model.close_connection()
